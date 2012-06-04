23cfbef2d83f07a27b4fa297b9f03abb7f94305c
Simon Pratt <spratt@scs.carleton.ca>
2012-06-04 17:54:45 -0400

    Added some documentation for the random point generation

--- generate_random_points.cpp
+++ generate_random_points.cpp
@@ -6,6 +6,7 @@ typedef CGAL::Random Random;
 using namespace std;
 
 int main(int argc, char** argv) {
+  // print a helpful message if not enough parameters given
   if(argc < 4) {
     cout << "Usage: " << argv[0] << " [n] [lower] [upper]" << endl
 	 << "Where" << endl
@@ -14,12 +15,14 @@ int main(int argc, char** argv) {
 	 << endl;
     return 0;
   }
+
+  // grab the parameters
   int n = atoi(argv[1]);
   int lower = atoi(argv[2]);
   int upper = atoi(argv[3]);
-  Random random;
 
   // print the points
+  Random random;
   while(--n >= 0) {
     cout << random.get_int(lower,upper) << " "
 	 << random.get_int(lower,upper) << endl;
--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ./build.sh
 
+if [ -f "generate_random_points" ]; then
+    ./generate_random_points 100 0 100000
+fi
+
 if [ -f "triangulate" -a -f "triangulate_input" ]; then
     cat triangulate_input | ./triangulate
 fi
