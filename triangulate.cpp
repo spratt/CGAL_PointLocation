a150a59022115a8db19842bb7ebd3f9294b3370e
Simon Pratt <spratt@scs.carleton.ca>
2012-06-04 14:17:18 -0400

    Initial commit

---
+++ triangulate.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Triangulation_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+typedef CGAL::Triangulation_2<K>             Triangulation;
+typedef Triangulation::Point                 Point;
+typedef Triangulation::Segment               Segment;
+typedef Triangulation::Finite_edges_iterator Edges_iterator;
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  istream_iterator<Point> begin(cin);
+  istream_iterator<Point> end;
+  
+  Triangulation t;
+  t.insert(begin, end);
+
+  Edges_iterator iter = t.finite_edges_begin();
+
+  while(iter != t.finite_edges_end()) {
+    cout << t.segment(iter) << endl;
+    ++iter;
+  }
+
+  return 0;
+}
