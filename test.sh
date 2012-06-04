7a0374129e9253392fd2139dab894d3b7272bf93
Simon Pratt <spratt@scs.carleton.ca>
2012-06-04 18:04:07 -0400

    Added some random_xmonotone_points test code

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-./build.sh
+./rebuild.sh
 
 if [ -f "generate_random_points" ]; then
     ./generate_random_points 100 0 100000
 fi
 
+if [ -f "random_xmonotone_points" ]; then
+    ./random_xmonotone_points 100 0 100000
+fi
+
 if [ -f "triangulate" -a -f "triangulate_input" ]; then
     cat triangulate_input | ./triangulate
+fi
+
+if [ -f "random_xmonotone_points" -a -f "triangulate" ]; then
+    ./random_xmonotone_points 100 0 100000 | ./triangulate
 fi
