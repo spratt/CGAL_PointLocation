b61b894e34a9ac6c07b7c4c18878b1d9dface353
Simon Pratt <spratt@scs.carleton.ca>
2012-06-04 18:16:31 -0400

    Removed unnecessary triangulation input, now that it can be generated

--- test.sh
+++ test.sh
@@ -9,10 +9,6 @@ if [ -f "random_xmonotone_points" ]; then
     ./random_xmonotone_points 100 0 100000
 fi
 
-if [ -f "triangulate" -a -f "triangulate_input" ]; then
-    cat triangulate_input | ./triangulate
-fi
-
 if [ -f "random_xmonotone_points" -a -f "triangulate" ]; then
     ./random_triangulation.sh 100 0 100000
 fi
