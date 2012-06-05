f5e6c0ae7fc4a71467c1f56d0e4e4816eea6cfdd
Simon Pratt <spratt@scs.carleton.ca>
2012-06-05 11:19:28 -0400

    Added locate_points to tests

--- test.sh
+++ test.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 ./rebuild.sh
 
-if [ -f "generate_random_points" ]; then
-    ./generate_random_points 100 0 100000
-fi
-
-if [ -f "random_xmonotone_points" ]; then
-    ./random_xmonotone_points 100 0 100000
-fi
-
 if [ -f "random_xmonotone_points" -a -f "triangulate" ]; then
-    ./random_triangulation.sh 100 0 100000
+    if [ -f "locate_points" -a -f "generate_random_points" ]; then
+	./random_triangulation.sh 5000 0 100000 > segments.txt
+	./generate_random_points 5000 0 100000 > points.txt
+	./locate_points segments.txt points.txt
+    fi
 fi
