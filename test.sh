e7f4e8bc652742b1e82c4c0c8c8e24a44b2446f3
Simon Pratt <spratt@scs.carleton.ca>
2012-06-08 17:20:37 -0400

    Can now show results of testing

--- test.sh
+++ test.sh
@@ -11,22 +11,26 @@ echo "======================================================================"
 echo "| Point Location Test                                                |"
 echo "======================================================================"
 
-if [ $# -gt 0 ]; then
-    TIMED_TEST=true
-else
-    TIMED_TEST=false
+TIMED=false
+SHOWN=false
+if [ $# -eq 1 ]; then
+    TIMED=true
+elif [ $# -gt 1 ]; then
+    SHOWN=true
 fi
 
-if [ -f "random_xmonotone_points" -a -f "triangulate" ]; then
-    if [ -f "locate_points" -a -f "generate_random_points" ]; then
-	echo "Generating triangulation..."
-	./random_triangulation.sh 5000 -100000 100000 > segments.txt
-	echo "Generating points..."
-	./generate_random_points 5000 -200000 200000 > points.txt
-	echo "Point location..."
-	if $TIMED_TEST; then
-	    time ./locate_points segments.txt points.txt > /dev/null
-	else
+if $TIMED; then
+    time ./locate_points segments.txt points.txt > /dev/null
+elif $SHOWN; then
+    time ./locate_points segments.txt points.txt
+else
+    if [ -f "random_xmonotone_points" -a -f "triangulate" ]; then
+	if [ -f "locate_points" -a -f "generate_random_points" ]; then
+	    echo "Generating triangulation..."
+	    ./random_triangulation.sh 5000 -100000 100000 > segments.txt
+	    echo "Generating points..."
+	    ./generate_random_points 5000 -200000 200000 > points.txt
+	    echo "Point location..."
 	    ./locate_points segments.txt points.txt
 	fi
     fi
